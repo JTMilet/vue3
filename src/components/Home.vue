9936cdcf8a01c4680ac34f104a2c8b784fe01431
杜康 <banshee1115@163.com>
2023-08-09 17:06:08 +0800

    增加tailwindcss框架

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,19 @@
 <!--
  * @Author: 杜康 banshee1115@163.com
- * @Date: 2023-08-09 16:02:01
+ * @Date: 2023-08-09 16:59:47
  * @LastEditors: 杜康 banshee1115@163.com
- * @LastEditTime: 2023-08-09 17:05:32
- * @FilePath: /vue3/src/App.vue
+ * @LastEditTime: 2023-08-09 17:02:35
+ * @FilePath: /vue3/src/components/home/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
+<template>
+  <div>
+    this is HTMLModElement
+  </div>
+</template>
+
 <script setup lang="ts">
-import Home from './components/Home.vue'
+
 </script>
 
-<template></template>
-
-<style scoped></style>
+<style lang="scss" scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: 杜康 banshee1115@163.com
+ * @Date: 2023-08-09 16:50:01
+ * @LastEditors: 杜康 banshee1115@163.com
+ * @LastEditTime: 2023-08-09 16:56:36
+ * @FilePath: /vue3/tailwind.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
